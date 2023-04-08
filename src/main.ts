7b1233a03645c02e671bd699b3f790365350682f
ginpachi987 <ginpachi987@gmail.com>
2023-04-08 10:41:42 +0700

    v0.0.2

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,12 @@ import App from './App.vue'
 
 import './assets/style.scss'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGear, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faGear, faXmark)
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
